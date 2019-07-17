2b23b9d2e09d031bb7d94e9a9f3f86613456c526
Rafael Oliveira <rafaelbatistacg@gmail.com>
2019-07-17 19:21:18 -0300

    Criei um controller, alterei propriedades da aplicação para Thymeleaf, adicionei home ao template, servidor exibindo home no localhost

---
+++ src/main/java/br/com/rafaeloliveira/gerenciadortarefas/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.rafaeloliveira.gerenciadortarefas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home/home";
+	}
+}
